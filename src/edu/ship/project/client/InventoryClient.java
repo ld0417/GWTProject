9c6e0c9b8682b5d89658ddda8c38a08712e7a46e
nk3668 <Nathaniel@157.160.142.21>
2014-12-06 16:36:25 -0500

    Having some problems with the edit button (and my code is very messy, I apologize, I need a few ideas for it :P)

--- src/edu/ship/project/client/InventoryClient.java
+++ src/edu/ship/project/client/InventoryClient.java
@@ -30,10 +30,102 @@ public class InventoryClient implements EntryPoint {
 		}
 	}
 	
-	public void deleteInventoryElement(int sku) { 
+	private void deleteInventoryElement(int sku) { 
 		int removedIndex = products.indexOf(sku);
         products.remove(removedIndex);
 		inventoryTable.removeRow(removedIndex+1);
+	}
+	
+	private void editInventoryElement(int sku) {
+		//TODO - get values from this row (row is from the number in the arraylist?)
+		// put remove Labels/widgets then insert a row of TextBoxes with the values 
+		// that were pulled in the beginning	
+		final int editIndex = products.indexOf(sku);
+		// Should SKU be able to change?
+		products.remove(editIndex);
+		//	If so remove it from the arraylist so it can be added again later (i.e. changed)
+		
+		//GET VALUES FROM LABELS
+		ArrayList<String> editProduct = new ArrayList<String>();
+		for(int col = 0; col < 5; col++) {
+			Widget widg = inventoryTable.getWidget(editIndex+1, col);
+			//System.out.println(widg.getClass().toString());
+			if(widg instanceof Label) {
+				String str =  ((Label)widg).getText();
+				editProduct.add(str);
+			}
+		}
+		
+		//REMOVE LABELS FROM TABLE
+		inventoryTable.removeRow(editIndex+2);
+		
+		//INSERT NEW ROW OF TEXTBOXES WITH ORIGINAL DATA
+		inventoryTable.insertRow(editIndex+2); // Make sure this is right
+		for(int col = 0; col < 5; col++) {
+			TextBox textBox = new TextBox();
+			textBox.getElement().setAttribute("value", editProduct.get(col));
+			inventoryTable.setWidget(editIndex, col, textBox);
+		}
+		
+		//ADD DONE BUTTON
+		Button doneButton = new Button("Done");
+		inventoryTable.setWidget(editIndex, 5, doneButton);
+		
+		//ADD ONCLICK - CHECK ALL COLUMNS AND DELETE ROW AND INSERT NEW ROW WITH LABELS
+		doneButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				//Change the TextBoxes back to Labels & Check if everything is filled
+				ArrayList<String> newProduct = new ArrayList<String>();
+				for(int col = 0; col < 5; col++) {
+					Widget widg = inventoryTable.getWidget(editIndex, col);
+					if(widg instanceof TextBox) {
+						String str =  ((TextBox)widg).getValue();
+						newProduct.add(str);
+					}
+				}
+				
+				//Ensure that all TextBoxes have the correct Values (Should check for SKU duplicates)
+				boolean textBoxesFull = true;
+				if(newProduct.size() > 0) {
+					for(String s : newProduct) {
+						if(s.equals("")) 
+							textBoxesFull = false;
+					}
+				}
+				
+				if(textBoxesFull) {
+					// First must remove the old one after extracting data
+					inventoryTable.removeRow(editIndex);
+					int tempRow = editIndex;
+					// Add new row to the FlexTable & update the products list
+					products.add(Integer.parseInt(newProduct.get(1)));
+					for(int col = 0; col < 7; col++) {
+						if(col < 5)
+							inventoryTable.setWidget(tempRow, col, new Label(newProduct.get(col)));
+						else {
+							Button editButton = new Button("Edit");
+							Button deleteButton = new Button("Delete");
+							
+							inventoryTable.setWidget(tempRow, 5, editButton);
+							inventoryTable.setWidget(tempRow, 6, deleteButton);
+							final int tempSKU = products.get(2);
+							
+							deleteButton.addClickHandler(new ClickHandler() { 
+								public void onClick(ClickEvent event) {
+									deleteInventoryElement(tempSKU);
+								}
+							});
+							
+							editButton.addClickHandler(new ClickHandler() { 
+								public void onClick(ClickEvent event) {
+									editInventoryElement(tempSKU);
+								}
+							});
+						}
+					}
+				}
+			}
+		});
 	}
 	
 	@Override
@@ -70,7 +162,7 @@ public class InventoryClient implements EntryPoint {
 			
 			editButton.addClickHandler(new ClickHandler() { 
 				public void onClick(ClickEvent event) {
-					//TODO - Edit method with some kind of interface
+					editInventoryElement(tempSKU);
 				}
 			});
 			products.add(row);
@@ -104,6 +196,7 @@ public class InventoryClient implements EntryPoint {
 							textBoxesFull = false;
 					}
 				}
+				
 				if(textBoxesFull) {
 					// First must remove the old one after extracting data
 					inventoryTable.removeRow(inventoryTable.getRowCount()-1);
@@ -129,7 +222,7 @@ public class InventoryClient implements EntryPoint {
 							
 							editButton.addClickHandler(new ClickHandler() { 
 								public void onClick(ClickEvent event) {
-									//TODO - Edit method with some kind of interface
+									editInventoryElement(tempSKU);
 								}
 							});
 						}
